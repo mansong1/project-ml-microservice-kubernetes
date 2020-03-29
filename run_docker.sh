b3a0b9b06e17c3f43850f12e9e0c1511da0445f8
Martin Ansong <mansong@Martins-MacBook.local>
2020-03-29 21:25:55 +0100

    Update run_docker.sh to run docker locally

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker build -f Dockerfile -t mansong/boston-housing:latest -t mansong/boston_housing:v1.0 .
 
-# Step 2: 
 # List docker images
+docker ps -a
 
-# Step 3: 
 # Run flask app
+docker run -d -p 5000:5000 mansong/boston_housing
