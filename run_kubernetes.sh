a17aba1faf7decfcb5810146c9b95718c43036dc
Martin Ansong <mansong@Martins-MacBook.local>
2020-03-30 19:10:53 +0100

    add docker and k8s logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,15 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mansong/boston-housing
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --generator=run-pod/v1 boston-housing --image=$dockerpath --port=80
 
-
-# Step 3:
 # List kubernetes pods
+kubectl wait pods/boston-housing --for=condition=complete
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward boston-housing 8000:80
