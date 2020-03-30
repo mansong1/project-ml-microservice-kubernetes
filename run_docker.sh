fc565ae1cc6320291b12c870ed9f231cc1910720
Martin Ansong <mansong@Martins-MacBook.local>
2020-03-30 17:47:35 +0100

    Update port

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,7 @@ docker image build -t boston-housing:latest .
 docker image ls
 
 # Run flask app
-docker run -d -p 5000:80 boston-housing:latest
+docker run -d -p 8000:80 boston-housing:latest
 
 # List docker process
 docker ps -a
