d9c8aef99a9a549620502464f79b11d11f79c1de
Martin Ansong <mansong@Martins-MacBook.local>
2020-04-02 18:38:46 +0100

    add static form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleFormSubmit( event ) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form action="/predict" method="post">
+          <h1>Boston House Predictor</h1>
+          <p>Enter values to predict value of owner occupied homes</p>
+          <label>CRIM - per capita crime rate by town</label>
+          <input type="text" name="crim" placeholder="CRIM" autoFocus required />
+          <input type="text" name="zn" placeholder="ZN" required />
+          <input type="text" name="indus" placeholder="INDUS" required/>
+          <input type="text" name="chas" placeholder="CHAS" required/>
+          <input type="text" name="nox" placeholder="NOX" required/>
+          <input type="text" name="rm" placeholder="RM" required/>
+          <input type="text" name="age" placeholder="AGE" required/>
+          <input type="text" name="dis" placeholder="DIS" required/>
+          <input type="text" name="rad" placeholder="RAD" required/>
+          <input type="text" name="tax" placeholder="TAX" required/>
+          <input type="text" name="ptratio" placeholder="PT RATIO" required/>
+          <input type="text" name="b" placeholder="B" required/>
+          <input type="text" name="lstat" placeholder="L STAT" required/>
+          <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Predict" />
+        </form>
     </div>
   );
 }
