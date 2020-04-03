28ba8fa1232300711dac8e539498b9a9ae6ba9d1
Martin Ansong <mansong@Martins-MacBook.local>
2020-04-03 08:23:14 +0100

    add function to handlechange

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
+
   function handleFormSubmit( event ) {
     event.preventDefault();
     console.log(this.state);
+  }
+  
+  function handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
   return (
@@ -12,21 +18,22 @@ function App() {
         <form action="/predict" method="post">
           <h1>Boston House Predictor</h1>
           <p>Enter values to predict value of owner occupied homes</p>
-          <label>CRIM - per capita crime rate by town</label>
-          <input type="text" name="crim" placeholder="CRIM" autoFocus required />
-          <input type="text" name="zn" placeholder="ZN" required />
-          <input type="text" name="indus" placeholder="INDUS" required/>
-          <input type="text" name="chas" placeholder="CHAS" required/>
-          <input type="text" name="nox" placeholder="NOX" required/>
-          <input type="text" name="rm" placeholder="RM" required/>
-          <input type="text" name="age" placeholder="AGE" required/>
-          <input type="text" name="dis" placeholder="DIS" required/>
-          <input type="text" name="rad" placeholder="RAD" required/>
-          <input type="text" name="tax" placeholder="TAX" required/>
-          <input type="text" name="ptratio" placeholder="PT RATIO" required/>
-          <input type="text" name="b" placeholder="B" required/>
-          <input type="text" name="lstat" placeholder="L STAT" required/>
-          <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Predict" />
+          <label>CRIM - per capita crime rate by town
+          <input type="text" name="crim" placeholder="CRIM" autoFocus value="0" onChange={e => handleChange(e)} />
+          </label>
+          <input type="text" name="zn" placeholder="ZN" value="0" />
+          <input type="text" name="indus" placeholder="INDUS" value="0"/>
+          <input type="text" name="chas" placeholder="CHAS" value="0"/>
+          <input type="text" name="nox" placeholder="NOX" value="0"/>
+          <input type="text" name="rm" placeholder="RM" value="0"/>
+          <input type="text" name="age" placeholder="AGE" value="0"/>
+          <input type="text" name="dis" placeholder="DIS" value="0"/>
+          <input type="text" name="rad" placeholder="RAD" value="0"/>
+          <input type="text" name="tax" placeholder="TAX" value="0"/>
+          <input type="text" name="ptratio" placeholder="PT RATIO" value="0"/>
+          <input type="text" name="b" placeholder="B" value="0"/>
+          <input type="text" name="lstat" placeholder="L STAT" value="0"/>
+          <input type="submit" onClick={e => handleFormSubmit(e)} value="Predict" />
         </form>
     </div>
   );
